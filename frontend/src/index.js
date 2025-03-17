e9a557b192422222194526eb94acd342a6f2c8ab
benjamin-ct <31733096+benjamin-ct@users.noreply.github.com>
2025-03-17 13:32:10 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 // frontend/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './app';
 import './index.css'; // Optionnel : pour ajouter du style global
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
